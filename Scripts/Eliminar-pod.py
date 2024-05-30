f681263b6258632cb92b5c4458df75d7d05298bc
Frank Harter <122062604+Frank-Harter@users.noreply.github.com>
2024-05-30 22:58:23 +0200

    Update Eliminar-pod.py

--- Scripts/Eliminar-pod.py
+++ Scripts/Eliminar-pod.py
@@ -1,8 +1,9 @@
 import subprocess
 import sys
+import pymysql
 
 server_name = sys.argv[1]
-
+nombre_usu = sys.argv[2]
 Eli_service = 'kubectl delete service ' + server_name
 subprocess.run(Eli_service, shell=True)
 
@@ -16,4 +17,28 @@ Eli_pv = 'kubectl delete pv ' + server_name
 subprocess.run(Eli_pv, shell=True)
 
 
+
+try:
+    conn = pymysql.connect(host='192.168.10.90', port=3307, user='admin', password='Super3ies1234.', db='FDominex', charset='utf8mb4')
+
+    #conn = pymysql.connect(host='192.168.10.200', user='root', password='Super3ies1234.', db='FDominex', charset='utf8mb4')
+    with conn.cursor() as cursor:
+        # Realizar una consulta
+        sele = f"SELECT puerto FROM FDominex.servidores WHERE dominio = '{server_name}';"
+        cursor.execute(sele)
+        result = cursor.fetchone()
+        puerto = result[0]
+        delete = f"""DELETE FROM FDominex.servidores WHERE dominio='{server_name}';"""
+        cursor.execute(delete)
+        conn.commit()
+        comando_pfsense = f"ssh -i /home/master/.ssh/id_rsa_new admin@192.168.10.1 'python3.11 /eliminar.py {puerto}'"
+        output = subprocess.run(comando_pfsense, shell=True, capture_output=True, text=True)
+        comando_samba = f"rm -r /mnt/samba/clientes/.{nombre_usu}/{server_name}"
+        output = subprocess.run(comando_samba, shell=True, capture_output=True, text=True)
+        comando_dns = f"ssh master@192.168.10.60 -i /home/master/.ssh/dnss 'python3 /etc/bind/zonas/delete-dns.py {server_name}'"
+        output = subprocess.run(comando_dns, shell=True, capture_output=True, text=True)
+
+
+finally:
+    conn.close()
 
